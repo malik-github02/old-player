8c5ce2852fb8eba96be94f06535974f5d9fbc4f0
Abd Almalik Kattan <76754847+malik-github02@users.noreply.github.com>
2022-06-24 00:34:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,31 @@ let btn = document.querySelector('.btn');
 let arm = document.querySelector('.arm');
 let recorder = document.querySelector('.recorder');
 let range = document.querySelector('#range');
-btn.onclick = function () {
-       if (recorder.classList.contains('on')) {
-              audioFile.pause();
+let turnKey = document.querySelector('.turn');
+let file = document.querySelector('#file');
+turnKey.onclick = function () {
+       if (turnKey.classList.contains('turn-on')) {
+              setTimeout(() => {
+                     audioFile.pause();
+                     recorder.classList.remove('on');
+              }, 500);
               arm.classList.remove('play-song');
-              recorder.classList.remove('on');
+              turnKey.classList.remove('turn-on');
        } else {
-              audioFile.play();
+              setTimeout(() => {
+                     audioFile.play();
+                     recorder.classList.add('on');
+              }, 500);
               arm.classList.add('play-song');
-              recorder.classList.add('on');
+              turnKey.classList.add('turn-on');
        }
+};
+file.onchange = function () {
+       let newFile = new FileReader();
+       newFile.readAsDataURL(file.files[0]);
+       newFile.onload = function () {
+              audioFile.src = newFile.result;
+       };
 };
 range.oninput = function () {
        audioFile.volume = range.value;
